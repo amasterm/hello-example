ee1bd0753db5add3fc99896fce1a0983651277b6
Andrew <appmarosek@interia.pl>
2018-07-02 17:32:27 +0200

    branch 5

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello2");
+
+
+
+    }
+
+    interface Phone{
+        void call();
+        void sms();
+    }
+    Class Iphone implements Phone{
+
     }
 }
